865c45f8b5307961116cda879ceefaaf6e3bd8a6
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-25 15:17:24 +0530

    Header Component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css"
+function Header(){
+    return(
+        <header>
+            <h1>⚡MOVIE-MANIA✨</h1>
+        </header>
+    )
+}
+
+export default Header;
