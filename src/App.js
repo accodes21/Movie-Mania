f7c302cf8101303ff3a8a3db37742fa3c65f5dc3
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-25 19:47:21 +0530

    Router elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { Container } from '@mui/material';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/Nav'
+import Movies from './Pages/Movies/Movies';
+import Series from './Pages/Series/Series';
+import Trending from './Pages/Trending/Trending';
+import Search from './Pages/Search/Search';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
     <Header/>
-    <main> abc </main>
+    <main> 
+    <Container>
+      <Routes>
+        <Route path="/" element={<Trending/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/series" element={<Series/>}/>
+        <Route path="/search" element={<Search/>}/>
+      </Routes>
+    </Container>
+    </main>
     <SimpleBottomNavigation/>
-    </>
+    </BrowserRouter>
   );
 }
 
