05a44a038a06a8ffa211cf4e13b691383f12934d
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-25 15:17:59 +0530

    Nav styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/Nav'
 
 function App() {
   return (
-    <></>
+    <>
+    <Header/>
+    <main> abc </main>
+    <SimpleBottomNavigation/>
+    </>
   );
 }
 
