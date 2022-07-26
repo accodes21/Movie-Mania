c8b0c74cfa90947fa7c9eeabf496a31c71128f43
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-26 20:00:40 +0530

    Height & Smooth Scrolling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Header.css"
+
 function Header(){
     return(
         <header>
-            <h1>⚡MOVIE-MANIA✨</h1>
+            <h1 onClick={() => window.scroll(0,0)}>⚡MOVIE-MANIA✨</h1>
         </header>
     )
 }
