f8a12841d466d6bda0b4ca6df2a1f77260cb1691
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-27 15:03:53 +0530

    Pagination

---
+++ src/components/Pagination/CustomPagination.jsx
@@ -0,0 +1,36 @@
+import { createTheme, Pagination, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    palette:{
+        mode:"dark"
+    }
+})
+
+const CustomPagination = ({setPage, numOfPages = 10}) => {
+
+    const handlePageChange = (page) => {
+        setPage(page);
+        window.scroll(0,0)
+    }
+    return(
+        <div
+        style={{
+            width:"100%",
+            display:"flex",
+            justifyContent:"center",
+            marginTop:"10px",
+            marginBottom:"10px"
+        }}
+        >
+        <ThemeProvider theme={theme}>
+            <Pagination 
+            count={numOfPages}
+            onChange={(e) => handlePageChange(e.target.textContent)}
+            color="primary"
+            />
+        </ThemeProvider>
+        </div>
+    )
+}
+
+export default CustomPagination;
