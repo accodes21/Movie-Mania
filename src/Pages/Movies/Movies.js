ac75480bfa19825744e6af16f082766ef90ca864
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-29 17:23:31 +0530

    Fully complete

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -47,7 +47,7 @@ const Movies = () => {
               poster={c.poster_path}
               title={c.title || c.name}
               date={c.first_air_date || c.release_date}
-              media_type={c.media_type}
+              media_type="movie"
               vote_average={c.vote_average}
             />
           ))}
