8baf98600237cf1d5e1f2249b3f977d3f0637810
Aarya Chopkar <aaryachopkar@gmail.com>
2023-04-03 17:04:35 +0530

    service worker added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,3 +13,5 @@ root.render(
   </React.StrictMode>
 );
 reportWebVitals();
+serviceWorkerRegistration.register();
+
